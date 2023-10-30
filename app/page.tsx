0a82bd3a030096649951ec6b31e8914c5860bbe6
codenamenam <bluetire27@gmail.com>
2023-10-30 22:51:59 +0900

    feat: Blueprint completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,122 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import { Button, Center, Flex, Text, Divider } from "@mantine/core";
+import Image from "next/image";
 
 export default function Home() {
+  interface rankingInfo {
+    schoolName: string;
+    ranking: number;
+    total: number;
+    totalScore: number;
+    average: number;
+  }
+
+  const myRanking: rankingInfo = {
+    schoolName: "반곡중학교",
+    ranking: 244,
+    total: 44,
+    totalScore: 424,
+    average: 9.4,
+  };
+
+  const rankings: rankingInfo[] = [
+    {
+      schoolName: "북원여자고등학교",
+      ranking: 1,
+      total: 43,
+      totalScore: 535,
+      average: 9.4,
+    },
+    {
+      schoolName: "원주여자고등학교",
+      ranking: 2,
+      total: 35,
+      totalScore: 252,
+      average: 9.3,
+    },
+    {
+      schoolName: "상지여자고등학교",
+      ranking: 3,
+      total: 455,
+      totalScore: 2442,
+      average: 9.1,
+    },
+  ];
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <Center>
+      <Flex
+        direction={"column"}
+        style={{
+          width: "340px",
+        }}
+        align={"center"}
+      >
+        <h1>전국 문해력 테스트!</h1>
+
+        <Button>
+          <Text>도전하기</Text>
+        </Button>
+
+        <Flex>
+          <Flex direction={"column"} align={"center"}>
+            <h1>내 점수</h1>
+            <div
+              style={{
+                backgroundColor: "grey",
+                width: "200px",
+                height: "100px",
+              }}
+            >
+              <Center>
+                <h1>721</h1>
+              </Center>
+            </div>
+          </Flex>
+
+          <Flex direction={"column"}>
+            <Button>교내 순위</Button>
+            <Button>올린 순위</Button>
+          </Flex>
+        </Flex>
+
+        <Flex direction={"column"} align={"center"}>
+          <h1>{myRanking["schoolName"]}</h1>
+          <Flex>
+            <Text>
+              {myRanking["ranking"]}위 | {myRanking["total"]}명
+              {myRanking["totalScore"]}점 | {myRanking["average"]}점
+            </Text>
+          </Flex>
+        </Flex>
+
+        {rankings.map((myRanking) => (
+          <Flex
+            direction={"column"}
+            align={"center"}
+            key={myRanking.schoolName}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+            <h1>
+              {myRanking.ranking}위 {myRanking.schoolName}
+            </h1>
+            <Flex>
+              <Text>
+                {myRanking.total}명 {myRanking.totalScore}점 |{" "}
+                {myRanking.average}점
+              </Text>
+            </Flex>
+          </Flex>
+        ))}
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <Flex direction={"column"} style={{ margin: "20px" }} align={"center"}>
+          <Text>테스트 공유하기</Text>
+          <Image
+            src={"/insta-logo.png"}
+            alt="instagram logo"
+            width={30}
+            height={30}
+          ></Image>
+        </Flex>
+      </Flex>
+    </Center>
+  );
 }
