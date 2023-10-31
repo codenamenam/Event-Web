0a11380671182ec667696febbeb1e25a5f9c5024
codenamenam <bluetire27@gmail.com>
2023-10-31 17:09:44 +0900

    feat: Capture and download

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button, Center, Flex, Text, SimpleGrid } from "@mantine/core";
+import { Button, Center, Flex, Text, Table, Divider } from "@mantine/core";
 import Image from "next/image";
 import Link from "next/link";
 import html2canvas from "html2canvas";
@@ -68,237 +68,301 @@ export default function Home() {
   };
 
   return (
-    <Center
-      style={{ backgroundImage: "url('/web-background.png')" }}
-      ref={divRef}
-    >
-      <Flex
-        direction={"column"}
-        style={{
-          width: "340px",
-          marginTop: "50px",
-        }}
-        align={"center"}
+    <div style={{ backgroundImage: "url('/web-background.png')" }}>
+      <Center
+        ref={divRef}
+        style={{ backgroundImage: "url('/web-background.png')" }}
       >
-        <Flex direction={"column"} align={"center"}>
-          <Text style={{ fontSize: "20px", fontWeight: "500", color: "gray" }}>
-            2023학년도
-          </Text>
-          <Text style={{ fontSize: "27px", fontWeight: "800" }}>
-            전국 문해력능력시험 성적통지표
-          </Text>
-        </Flex>
-
-        <Button
-          component={Link}
-          href="https://pf.kakao.com/_zmTAG/friend"
-          variant="outline"
-          radius={"xl"}
-          style={{ margin: "10px 0 30px 0", width: "20 0px" }}
-          color="red"
-        >
-          <Text style={{ fontWeight: "700", fontSize: "18px" }}>
-            능력시험 응시하기
-          </Text>
-        </Button>
-        <div
+        <Flex
+          direction={"column"}
           style={{
-            backgroundColor: "white",
-            padding: "20px",
-            borderRadius: "10px",
+            width: "340px",
+            marginTop: "50px",
           }}
+          align={"center"}
         >
           <Flex direction={"column"} align={"center"}>
             <Text
-              style={{
-                fontSize: "15px",
-                fontWeight: "600",
-              }}
+              style={{ fontSize: "20px", fontWeight: "500", color: "gray" }}
             >
-              나의 학교
+              2023학년도
             </Text>
-            <Text
-              style={{
-                fontSize: "30px",
-                fontWeight: "700",
-                marginBottom: "10px",
-              }}
-            >
-              {myRanking["schoolName"]}
+            <Text style={{ fontSize: "27px", fontWeight: "800" }}>
+              전국 문해력능력시험 성적통지표
             </Text>
           </Flex>
-          <Flex align={"center"}>
-            <Flex direction={"column"} align={"center"}>
-              <Center
-                style={{
-                  backgroundColor: "rgba(0,0,0,0.05)",
-                  width: "100px",
-                  height: "30px",
-                }}
-              >
-                <Text style={{ fontSize: "15px", fontWeight: "600" }}>
-                  학교 순위
-                </Text>
-              </Center>
-              <Center
-                style={{
-                  width: "100px",
-                  height: "50px",
-                  backgroundColor: "rgba(0,0,0,0.02)",
-                }}
-              >
-                <Text style={{ fontSize: "20px", fontWeight: "500" }}>
-                  {myRanking["ranking"]}위
-                </Text>
-              </Center>
-            </Flex>
-            <Flex direction={"column"} align={"center"}>
-              <Center
-                style={{
-                  backgroundColor: "rgba(0,0,0,0.05)",
-                  width: "100px",
-                  height: "30px",
-                }}
-              >
-                <Text style={{ fontSize: "15px", fontWeight: "600" }}>
-                  내 점수
-                </Text>
-              </Center>
-              <Center
-                style={{
-                  width: "100px",
-                  height: "50px",
-                  backgroundColor: "rgba(0,0,0,0.02)",
-                }}
-              >
-                <Text style={{ fontSize: "20px", fontWeight: "500" }}>9.6</Text>
-              </Center>
-            </Flex>
-            <Flex direction={"column"} align={"center"}>
-              <Center
-                style={{
-                  backgroundColor: "rgba(0,0,0,0.05)",
-                  width: "100px",
-                  height: "30px",
-                }}
-              >
-                <Text style={{ fontSize: "15px", fontWeight: "600" }}>
-                  교내 순위
-                </Text>
-              </Center>
 
-              <Center
-                style={{
-                  width: "100px",
-                  height: "50px",
-                  backgroundColor: "rgba(0,0,0,0.02)",
-                }}
-              >
-                <Text style={{ fontSize: "20px", fontWeight: "500" }}>2등</Text>
-              </Center>
-            </Flex>
-          </Flex>
-
-          <Flex align={"center"}>
+          <Button
+            component={Link}
+            href="https://pf.kakao.com/_zmTAG/chat"
+            variant="outline"
+            radius={"xl"}
+            style={{ margin: "10px 0 30px 0", width: "20 0px" }}
+            color="red"
+          >
+            <Text style={{ fontWeight: "700", fontSize: "18px" }}>
+              시험 응시하기
+            </Text>
+          </Button>
+          <div
+            style={{
+              backgroundColor: "white",
+              padding: "20px",
+              borderRadius: "10px",
+            }}
+          >
             <Flex direction={"column"} align={"center"}>
-              <Center
+              <Text
                 style={{
-                  backgroundColor: "rgba(0,0,0,0.05)",
-                  width: "100px",
-                  height: "30px",
+                  fontSize: "15px",
+                  fontWeight: "600",
                 }}
               >
-                <Text style={{ fontSize: "15px", fontWeight: "600" }}>
-                  전체 참여자수
-                </Text>
-              </Center>
-              <Center
+                나의 학교
+              </Text>
+              <Text
                 style={{
-                  width: "100px",
-                  height: "50px",
-                  backgroundColor: "rgba(0,0,0,0.02)",
+                  fontSize: "30px",
+                  fontWeight: "700",
+                  marginBottom: "10px",
                 }}
               >
-                <Text style={{ fontSize: "20px", fontWeight: "500" }}>
-                  {myRanking["total"]}명
-                </Text>
-              </Center>
+                {myRanking["schoolName"]}
+              </Text>
             </Flex>
-            <Flex direction={"column"} align={"center"}>
-              <Center
-                style={{
-                  backgroundColor: "rgba(0,0,0,0.05)",
-                  width: "100px",
-                  height: "30px",
-                }}
-              >
-                <Text style={{ fontSize: "15px", fontWeight: "600" }}>
-                  총점
-                </Text>
-              </Center>
-              <Center
-                style={{
-                  width: "100px",
-                  height: "50px",
-                  backgroundColor: "rgba(0,0,0,0.02)",
-                }}
-              >
-                <Text style={{ fontSize: "20px", fontWeight: "500" }}>
-                  {myRanking["totalScore"]}점
-                </Text>
-              </Center>
+            <Flex align={"center"}>
+              <Flex direction={"column"} align={"center"}>
+                <Center
+                  style={{
+                    backgroundColor: "rgba(0,0,0,0.05)",
+                    width: "100px",
+                    height: "30px",
+                  }}
+                >
+                  <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+                    학교 순위
+                  </Text>
+                </Center>
+                <Center
+                  style={{
+                    width: "100px",
+                    height: "50px",
+                    backgroundColor: "rgba(0,0,0,0.02)",
+                  }}
+                >
+                  <Text style={{ fontSize: "20px", fontWeight: "500" }}>
+                    {myRanking["ranking"]}위
+                  </Text>
+                </Center>
+              </Flex>
+              <Flex direction={"column"} align={"center"}>
+                <Center
+                  style={{
+                    backgroundColor: "rgba(0,0,0,0.05)",
+                    width: "100px",
+                    height: "30px",
+                  }}
+                >
+                  <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+                    내 점수
+                  </Text>
+                </Center>
+                <Center
+                  style={{
+                    width: "100px",
+                    height: "50px",
+                    backgroundColor: "rgba(0,0,0,0.02)",
+                  }}
+                >
+                  <Text style={{ fontSize: "20px", fontWeight: "500" }}>
+                    9.6
+                  </Text>
+                </Center>
+              </Flex>
+              <Flex direction={"column"} align={"center"}>
+                <Center
+                  style={{
+                    backgroundColor: "rgba(0,0,0,0.05)",
+                    width: "100px",
+                    height: "30px",
+                  }}
+                >
+                  <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+                    교내 순위
+                  </Text>
+                </Center>
+
+                <Center
+                  style={{
+                    width: "100px",
+                    height: "50px",
+                    backgroundColor: "rgba(0,0,0,0.02)",
+                  }}
+                >
+                  <Text style={{ fontSize: "20px", fontWeight: "500" }}>
+                    2등
+                  </Text>
+                </Center>
+              </Flex>
             </Flex>
-            <Flex direction={"column"} align={"center"}>
-              <Center
-                style={{
-                  backgroundColor: "rgba(0,0,0,0.05)",
-                  width: "100px",
-                  height: "30px",
-                }}
-              >
-                <Text style={{ fontSize: "15px", fontWeight: "600" }}>
-                  평균 점수
-                </Text>
-              </Center>
 
-              <Center
-                style={{
-                  backgroundColor: "rgba(0,0,0,0.02)",
-                  width: "100px",
-                  height: "50px",
-                }}
-              >
-                <Text style={{ fontSize: "20px", fontWeight: "500" }}>
-                  {myRanking["average"]}점
-                </Text>
-              </Center>
+            <Flex align={"center"}>
+              <Flex direction={"column"} align={"center"}>
+                <Center
+                  style={{
+                    backgroundColor: "rgba(0,0,0,0.05)",
+                    width: "100px",
+                    height: "30px",
+                  }}
+                >
+                  <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+                    전체 참여자수
+                  </Text>
+                </Center>
+                <Center
+                  style={{
+                    width: "100px",
+                    height: "50px",
+                    backgroundColor: "rgba(0,0,0,0.02)",
+                  }}
+                >
+                  <Text style={{ fontSize: "20px", fontWeight: "500" }}>
+                    {myRanking["total"]}명
+                  </Text>
+                </Center>
+              </Flex>
+              <Flex direction={"column"} align={"center"}>
+                <Center
+                  style={{
+                    backgroundColor: "rgba(0,0,0,0.05)",
+                    width: "100px",
+                    height: "30px",
+                  }}
+                >
+                  <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+                    총점
+                  </Text>
+                </Center>
+                <Center
+                  style={{
+                    width: "100px",
+                    height: "50px",
+                    backgroundColor: "rgba(0,0,0,0.02)",
+                  }}
+                >
+                  <Text style={{ fontSize: "20px", fontWeight: "500" }}>
+                    {myRanking["totalScore"]}점
+                  </Text>
+                </Center>
+              </Flex>
+              <Flex direction={"column"} align={"center"}>
+                <Center
+                  style={{
+                    backgroundColor: "rgba(0,0,0,0.05)",
+                    width: "100px",
+                    height: "30px",
+                  }}
+                >
+                  <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+                    평균 점수
+                  </Text>
+                </Center>
+
+                <Center
+                  style={{
+                    backgroundColor: "rgba(0,0,0,0.02)",
+                    width: "100px",
+                    height: "50px",
+                  }}
+                >
+                  <Text style={{ fontSize: "20px", fontWeight: "500" }}>
+                    {myRanking["average"]}점
+                  </Text>
+                </Center>
+              </Flex>
             </Flex>
-          </Flex>
-        </div>
+          </div>
 
-        {rankings.map((ranking) => (
-          <SimpleGrid cols={5} key={ranking.schoolName}>
-            <div>{ranking.ranking}</div>
-            <div>{ranking.schoolName}</div>
-            <div>{ranking.total}</div>
-            <div>{ranking.totalScore}</div>
-            <div>{ranking.average}</div>
-          </SimpleGrid>
-        ))}
-
-        <Flex direction={"column"} style={{ margin: "20px" }} align={"center"}>
-          <Text style={{ fontSize: "23px", fontWeight: "700", margin: "10px" }}>
-            테스트 공유하기
-          </Text>
-          <Image
-            src={"/insta-logo.png"}
-            alt="instagram logo"
-            width={30}
-            height={30}
-            onClick={handleDownload}
-          ></Image>
+          <Table
+            style={{
+              marginTop: "30px",
+            }}
+          >
+            <Table.Thead>
+              <Table.Th style={{ textAlign: "center" }}>등수</Table.Th>
+              <Table.Th style={{ textAlign: "center" }}>학교 이름</Table.Th>
+              <Table.Th style={{ textAlign: "center" }}>참여자 수 </Table.Th>
+              <Table.Th style={{ textAlign: "center" }}>총점</Table.Th>
+              <Table.Th style={{ textAlign: "center" }}>평균</Table.Th>
+            </Table.Thead>
+            <Table.Tbody>
+              {rankings.map((element) => (
+                <Table.Tr key={element.schoolName}>
+                  <Table.Td style={{ textAlign: "center" }}>
+                    {element.ranking}
+                  </Table.Td>
+                  <Table.Td style={{ textAlign: "center" }}>
+                    {element.schoolName}
+                  </Table.Td>
+                  <Table.Td style={{ textAlign: "center" }}>
+                    {element.total}
+                  </Table.Td>
+                  <Table.Td style={{ textAlign: "center" }}>
+                    {element.totalScore}
+                  </Table.Td>
+                  <Table.Td style={{ textAlign: "center" }}>
+                    {element.average}
+                  </Table.Td>
+                </Table.Tr>
+              ))}
+            </Table.Tbody>
+          </Table>
         </Flex>
-      </Flex>
-    </Center>
+      </Center>
+      <Center>
+        <Flex direction="column" style={{ margin: "20px", width: "330px" }}>
+          <Text
+            style={{
+              fontSize: "16px",
+              color: "gray",
+              fontWeight: "600",
+              margin: "5px 0 0 0",
+            }}
+          >
+            테스트 결과 공유하기
+          </Text>
+
+          <Divider style={{ margin: "3px 0px 9px 0px" }} />
+
+          <Flex direction={"column"}>
+            <Flex justify={"space-between"}>
+              <Text>1. 버튼을 눌러서 화면 캡처본을 다운로드!</Text>
+            </Flex>
+            <Text>2. 카카오톡, 인스타 등 SNS에 공유하기!</Text>
+          </Flex>
+
+          <Center style={{ margin: "10px 0 30px 0" }}>
+            <Flex style={{ width: "150px" }} justify={"space-between"}>
+              <Image
+                src={"/download-icon.png"}
+                alt="instagram logo"
+                width={35}
+                height={35}
+                onClick={handleDownload}
+              ></Image>
+              <Image
+                src={"/insta-logo.png"}
+                alt="instagram logo"
+                width={30}
+                height={30}
+                onClick={() => {
+                  window.location.href = "instagram-stories://share";
+                }}
+              ></Image>
+            </Flex>
+          </Center>
+        </Flex>
+      </Center>
+    </div>
   );
 }
