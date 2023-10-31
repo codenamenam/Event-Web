1575b5794420d5a33d629c0bc4a6ae3fce2e9238
codenamenam <bluetire27@gmail.com>
2023-10-31 12:50:58 +0900

    fix: test

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Button, Center, Flex, Text, Divider } from "@mantine/core";
+import { Button, Center, Flex, Text, Grid } from "@mantine/core";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -44,126 +44,234 @@ export default function Home() {
   ];
 
   return (
-    <Center style={{ backgroundImage: "url('/web-background.png')" }}>
-      <Flex
-        direction={"column"}
-        style={{
-          width: "340px",
-          marginTop: "50px",
-        }}
-        align={"center"}
-      >
-        <Flex direction={"column"} align={"center"}>
-          <Text style={{ fontSize: "20px", fontWeight: "500", color: "gray" }}>
-            2023학년도
-          </Text>
-          <Text style={{ fontSize: "27px", fontWeight: "800" }}>
-            전국 문해력능력시험 성적통지표
-          </Text>
-        </Flex>
-
-        <Button
-          component={Link}
-          href="https://pf.kakao.com/_zmTAG/friend"
-          variant="outline"
-          radius={"xl"}
-          style={{ margin: "20px", width: "20 0px" }}
-          color="red"
-        >
-          <Text style={{ fontWeight: "700", fontSize: "18px" }}>
-            능력시험 응시하기
-          </Text>
-        </Button>
-
-        <Flex direction={"column"} align={"center"}>
-          <Text>출신학교</Text>
-          <Text style={{ fontSize: "30px", fontWeight: "700" }}>
-            {myRanking["schoolName"]}
-          </Text>
-        </Flex>
-        <Flex align={"center"}>
-          <Flex direction={"column"} align={"center"}>
-            <Text style={{ fontSize: "15px", fontWeight: "600" }}>
-              학교 순위
-            </Text>
-            <Center
-              style={{
-                backgroundColor: "grey",
-                width: "100px",
-                height: "50px",
-              }}
-            >
-              <Text style={{ fontSize: "20px", fontWeight: "500" }}>
-                {myRanking["ranking"]}위
-              </Text>
-            </Center>
-          </Flex>
-          <Flex direction={"column"} align={"center"}>
-            <Text style={{ fontSize: "15px", fontWeight: "600" }}>내 점수</Text>
-            <Center
-              style={{
-                backgroundColor: "grey",
-                width: "100px",
-                height: "50px",
-              }}
-            >
-              <Text style={{ fontSize: "20px", fontWeight: "500" }}>9.6</Text>
-            </Center>
-          </Flex>
-          <Flex direction={"column"} align={"center"}>
-            <Text style={{ fontSize: "15px", fontWeight: "600" }}>
-              교내 순위
-            </Text>
-
-            <Center
-              style={{
-                backgroundColor: "grey",
-                width: "100px",
-                height: "50px",
-              }}
-            >
-              <Text style={{ fontSize: "20px", fontWeight: "500" }}>2등</Text>
-            </Center>
-          </Flex>
-        </Flex>
-
-        <Flex direction={"column"} align={"center"}>
-          <Flex>
-            <Text>
-              {myRanking["total"]}명 | {myRanking["totalScore"]}점 |{" "}
-              {myRanking["average"]}점
-            </Text>
-          </Flex>
-        </Flex>
-
-        {rankings.map((myRanking) => (
-          <Flex
-            direction={"column"}
-            align={"center"}
-            key={myRanking.schoolName}
-          >
-            <h1>
-              {myRanking.ranking}위 {myRanking.schoolName}
-            </h1>
-            <Flex>
-              <Text>
-                {myRanking.total}명 | {myRanking.totalScore}점 |{" "}
-                {myRanking.average}점
-              </Text>
-            </Flex>
-          </Flex>
-        ))}
-
-        <Flex direction={"column"} style={{ margin: "20px" }} align={"center"}>
-          <Text>테스트 공유하기</Text>
-          <Image
-            src={"/insta-logo.png"}
-            alt="instagram logo"
-            width={30}
-            height={30}
-          ></Image>
-        </Flex>
-      </Flex>
-    </Center>
+    <div>
+      <Grid>
+        <Grid.Col span={3}>1</Grid.Col>
+        <Grid.Col span={3}>2</Grid.Col>
+        <Grid.Col span={3}>3</Grid.Col>
+        <Grid.Col span={3}>4</Grid.Col>
+      </Grid>
+    </div>
   );
+
+  // return (
+  //   <Center style={{ backgroundImage: "url('/web-background.png')" }}>
+  //     <Flex
+  //       direction={"column"}
+  //       style={{
+  //         width: "340px",
+  //         marginTop: "50px",
+  //       }}
+  //       align={"center"}
+  //     >
+  //       <Flex direction={"column"} align={"center"}>
+  //         <Text style={{ fontSize: "20px", fontWeight: "500", color: "gray" }}>
+  //           2023학년도
+  //         </Text>
+  //         <Text style={{ fontSize: "27px", fontWeight: "800" }}>
+  //           전국 문해력능력시험 성적통지표
+  //         </Text>
+  //       </Flex>
+
+  //       <Button
+  //         component={Link}
+  //         href="https://pf.kakao.com/_zmTAG/friend"
+  //         variant="outline"
+  //         radius={"xl"}
+  //         style={{ margin: "10px 0 30px 0", width: "20 0px" }}
+  //         color="red"
+  //       >
+  //         <Text style={{ fontWeight: "700", fontSize: "18px" }}>
+  //           능력시험 응시하기
+  //         </Text>
+  //       </Button>
+  //       <div
+  //         style={{
+  //           backgroundColor: "white",
+  //           padding: "20px",
+  //           borderRadius: "10px",
+  //         }}
+  //       >
+  //         <Flex direction={"column"} align={"center"}>
+  //           <Text
+  //             style={{
+  //               fontSize: "15px",
+  //               fontWeight: "600",
+  //             }}
+  //           >
+  //             나의 학교
+  //           </Text>
+  //           <Text
+  //             style={{
+  //               fontSize: "30px",
+  //               fontWeight: "700",
+  //               marginBottom: "10px",
+  //             }}
+  //           >
+  //             {myRanking["schoolName"]}
+  //           </Text>
+  //         </Flex>
+  //         <Flex align={"center"}>
+  //           <Flex direction={"column"} align={"center"}>
+  //             <Center
+  //               style={{
+  //                 backgroundColor: "rgba(0,0,0,0.05)",
+  //                 width: "100px",
+  //                 height: "30px",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+  //                 학교 순위
+  //               </Text>
+  //             </Center>
+  //             <Center
+  //               style={{
+  //                 width: "100px",
+  //                 height: "50px",
+  //                 backgroundColor: "rgba(0,0,0,0.02)",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "20px", fontWeight: "500" }}>
+  //                 {myRanking["ranking"]}위
+  //               </Text>
+  //             </Center>
+  //           </Flex>
+  //           <Flex direction={"column"} align={"center"}>
+  //             <Center
+  //               style={{
+  //                 backgroundColor: "rgba(0,0,0,0.05)",
+  //                 width: "100px",
+  //                 height: "30px",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+  //                 내 점수
+  //               </Text>
+  //             </Center>
+  //             <Center
+  //               style={{
+  //                 width: "100px",
+  //                 height: "50px",
+  //                 backgroundColor: "rgba(0,0,0,0.02)",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "20px", fontWeight: "500" }}>9.6</Text>
+  //             </Center>
+  //           </Flex>
+  //           <Flex direction={"column"} align={"center"}>
+  //             <Center
+  //               style={{
+  //                 backgroundColor: "rgba(0,0,0,0.05)",
+  //                 width: "100px",
+  //                 height: "30px",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+  //                 교내 순위
+  //               </Text>
+  //             </Center>
+
+  //             <Center
+  //               style={{
+  //                 width: "100px",
+  //                 height: "50px",
+  //                 backgroundColor: "rgba(0,0,0,0.02)",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "20px", fontWeight: "500" }}>2등</Text>
+  //             </Center>
+  //           </Flex>
+  //         </Flex>
+
+  //         <Flex align={"center"}>
+  //           <Flex direction={"column"} align={"center"}>
+  //             <Center
+  //               style={{
+  //                 backgroundColor: "rgba(0,0,0,0.05)",
+  //                 width: "100px",
+  //                 height: "30px",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+  //                 전체 참여자수
+  //               </Text>
+  //             </Center>
+  //             <Center
+  //               style={{
+  //                 width: "100px",
+  //                 height: "50px",
+  //                 backgroundColor: "rgba(0,0,0,0.02)",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "20px", fontWeight: "500" }}>
+  //                 {myRanking["total"]}명
+  //               </Text>
+  //             </Center>
+  //           </Flex>
+  //           <Flex direction={"column"} align={"center"}>
+  //             <Center
+  //               style={{
+  //                 backgroundColor: "rgba(0,0,0,0.05)",
+  //                 width: "100px",
+  //                 height: "30px",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+  //                 총점
+  //               </Text>
+  //             </Center>
+  //             <Center
+  //               style={{
+  //                 width: "100px",
+  //                 height: "50px",
+  //                 backgroundColor: "rgba(0,0,0,0.02)",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "20px", fontWeight: "500" }}>
+  //                 {myRanking["totalScore"]}점
+  //               </Text>
+  //             </Center>
+  //           </Flex>
+  //           <Flex direction={"column"} align={"center"}>
+  //             <Center
+  //               style={{
+  //                 backgroundColor: "rgba(0,0,0,0.05)",
+  //                 width: "100px",
+  //                 height: "30px",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "15px", fontWeight: "600" }}>
+  //                 평균 점수
+  //               </Text>
+  //             </Center>
+
+  //             <Center
+  //               style={{
+  //                 backgroundColor: "rgba(0,0,0,0.02)",
+  //                 width: "100px",
+  //                 height: "50px",
+  //               }}
+  //             >
+  //               <Text style={{ fontSize: "20px", fontWeight: "500" }}>
+  //                 {myRanking["average"]}점
+  //               </Text>
+  //             </Center>
+  //           </Flex>
+  //         </Flex>
+  //       </div>
+
+  //       <Flex direction={"column"} style={{ margin: "20px" }} align={"center"}>
+  //         <Text style={{ fontSize: "23px", fontWeight: "700", margin: "10px" }}>
+  //           테스트 공유하기
+  //         </Text>
+  //         <Image
+  //           src={"/insta-logo.png"}
+  //           alt="instagram logo"
+  //           width={30}
+  //           height={30}
+  //         ></Image>
+  //       </Flex>
+  //     </Flex>
+  //   </Center>
+  // );
 }
