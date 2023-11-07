8f0b5a8faf879b7a17c0aa6e1ded9428e5a16acb
codenamenam <bluetire27@gmail.com>
2023-11-07 19:12:56 +0900

    fix: not working supabase rpc

--- app/[user_id]/api/getTopSchoolRanking/route.ts
+++ app/[user_id]/api/getTopSchoolRanking/route.ts
@@ -7,7 +7,7 @@ export async function GET(request: NextRequest) {
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVibmV5Y2Jxd3R1aHl4Z2dnaGlhIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4OTE0MDY2OCwiZXhwIjoyMDA0NzE2NjY4fQ.s6hubrc6xEmKnz2Z7f-QxgOq9sJNN19d8fqtosCoPDs"
   );
 
-  const { data, error } = await supabase.rpc("get_top_school_ranking_data_web");
+  const { data, error } = await supabase.rpc("get_top10_school_ranking");
   console.log(data);
   return NextResponse.json(data, {
     status: 200,
